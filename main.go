8b5216e6eeacfa6c00b56298ccb446beb02c2e3a
Pedro Oliveira <pedro.dev.oliveira@gmail.com>
2023-08-07 20:10:26 -0300

    :construction: add date formatting

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"Store/routes"
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	routes.LoadRoutes()
+	fmt.Print("Server is running on http://localhost:8000")
 	http.ListenAndServe(":8000", nil)
 }
